47ac37b67b06fe0b7888e1769da29eb41b6d0481
beth-ann-tucker <142539226+beth-ann-tucker@users.noreply.github.com>
2023-10-04 18:40:27 -0500

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
- $(function () {
+//code that hides the professional to begin with and allows user to toggle between professional and personal
+$(function () {
   console.log("ready!");
 
   $("#professionalDiv").hide();
